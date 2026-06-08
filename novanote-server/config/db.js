57fa262d0f5db16c585273ab382d9d5a3aa0cdfd
411277002 <411277002@mail.nknu.edu.tw>
2026-06-08 11:45:46 +0800

    Fix database port config

--- novanote-server/config/db.js
+++ novanote-server/config/db.js
@@ -1,13 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+})
 
-module.exports = pool.promise();
+module.exports = pool.promise()
